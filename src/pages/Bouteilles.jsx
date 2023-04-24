65f7140dd81b2e710621af8e3544aa720b6d3118
Lucas <lucas.soaresmoenner@gmail.com>
2023-04-24 12:26:50 +0200

    feat: front bouteilles

--- src/pages/Bouteilles.jsx
+++ src/pages/Bouteilles.jsx
@@ -14,7 +14,15 @@ import {
   Thead,
   Button,
   useColorModeValue,
+  Stack,
+  Center,
+  HStack,
+  Grid,
 } from "@chakra-ui/react";
+import HospicesDeBeaune from "../assets/HospicesDeBeaune.jpg";
+import cognac from "../assets/cognac.jpg";
+import tariquet from "../assets/tariquet.jpg";
+
 
 function BottleSettings(props) {
   return (
@@ -22,7 +30,6 @@ function BottleSettings(props) {
       <Header />
       <Box
         minH={"84.6vh"}
-        justify={"center"}
         bg={useColorModeValue("gray.50", "gray.800")}
       >
         <Flex justify={"space-between"} align={"center"} ml={20} mr={20} pt={10}>
@@ -42,7 +49,230 @@ function BottleSettings(props) {
             </Link>
           </Box>
         </Flex>
+        <Grid templateColumns="repeat(3, 1fr)" pt={20} gap={6}>
+          <Box w="100%" h="100%">
+            <Center py={6}>
+              <Link href="/one-bottle-settings">
+              <Box
+                width={200}
+                height={230}
+                left={245}
+                borderRadius={22}
+                maxW={"270px"}
+                bg={"white"}
+                boxShadow={"lg"}
+                overflow={"hidden"}
+                p={6}
+              >
+                <Stack spacing={0} align={"center"} mb={5}>
+                  <Heading
+                  width={147}
+                  height={130}
+                  left={919}
+                  top={637}
+                    fontSize={"6xl"}
+                    fontWeight={500}
+                    mb={5}
+                    fontFamily={"body"}
+                  >
+                    <img 
+                      src={tariquet}
+                      alt="pt_menu"
+                     />
+                  </Heading>
+
+                  <p>Tariquet</p>
+                </Stack>
+              </Box>
+              </Link>
+            </Center>
+          </Box>              
+          
+          <Box w="100%" h="100%">
+            <Center py={6}>
+            <Box
+                width={200}
+                height={230}
+                left={245}
+                top={637}
+                borderRadius={22}
+                maxW={"270px"}
+                bg={"white"}
+                boxShadow={"lg"}
+                overflow={"hidden"}
+                p={6}
+              >
+                <Stack spacing={0} align={"center"} mb={5}>
+                  <Heading
+                  width={147}
+                  height={130}
+                  left={919}
+                  top={637}
+                    fontSize={"6xl"}
+                    fontWeight={500}
+                    mb={5}
+                    fontFamily={"body"}
+                  >
+                     <img 
+                      src={HospicesDeBeaune}
+                      alt="pt_menu"
+                     />
+                  </Heading>
+
+                  <p>Hospices de Beaune</p>
+                </Stack>
+              </Box>
+            </Center>
+          </Box>
+          <Box w="100%" h="100%">
+            <Center py={6}>
+            <Box
+                width={200}
+                height={230}
+                left={245}
+                top={637}
+                borderRadius={22}
+                maxW={"270px"}
+                bg={"white"}
+                boxShadow={"lg"}
+                overflow={"hidden"}
+                p={6}
+              >
+                <Stack spacing={0} align={"center"} mb={5}>
+                  <Heading
+                  width={147}
+                  height={130}
+                  left={919}
+                  top={637}
+                    fontSize={"6xl"}
+                    fontWeight={500}
+                    mb={5}
+                    fontFamily={"body"}
+                  >
+                     <img 
+                      src={cognac}
+                      alt="pt_menu"
+                     />
+                  </Heading>
+
+                  <p>Cognac</p>
+                </Stack>
+              </Box>
+            </Center>
+          </Box>
+      </Grid>
+      <Grid templateColumns="repeat(3, 1fr)" pt={20} gap={6}>
+          <Box w="100%" h="100%">
+            <Center py={6}>
+              <Box
+                width={200}
+                height={230}
+                left={245}
+                borderRadius={22}
+                maxW={"270px"}
+                bg={"white"}
+                boxShadow={"lg"}
+                overflow={"hidden"}
+                p={6}
+              >
+                <Stack spacing={0} align={"center"} mb={5}>
+                  <Heading
+                  width={147}
+                  height={130}
+                  left={919}
+                  top={637}
+                    fontSize={"6xl"}
+                    fontWeight={500}
+                    mb={5}
+                    fontFamily={"body"}
+                  >
+                    <img 
+                      src={tariquet}
+                      alt="pt_menu"
+                     />
+                  </Heading>
+
+                  <p>Tariquet</p>
+                </Stack>
+              </Box>
+            </Center>
+          </Box>
+          <Box w="100%" h="100%">
+            <Center py={6}>
+            <Box
+                width={200}
+                height={230}
+                left={245}
+                top={637}
+                borderRadius={22}
+                maxW={"270px"}
+                bg={"white"}
+                boxShadow={"lg"}
+                overflow={"hidden"}
+                p={6}
+              >
+                <Stack spacing={0} align={"center"} mb={5}>
+                  <Heading
+                  width={147}
+                  height={130}
+                  left={919}
+                  top={637}
+                    fontSize={"6xl"}
+                    fontWeight={500}
+                    mb={5}
+                    fontFamily={"body"}
+                  >
+                     <img 
+                      src={HospicesDeBeaune}
+                      alt="pt_menu"
+                     />
+                  </Heading>
+
+                  <p>Hospices de Beaune</p>
+                </Stack>
+              </Box>
+            </Center>
+          </Box>
+          <Box w="100%" h="100%">
+            <Center py={6}>
+            <Box
+                width={200}
+                height={230}
+                left={245}
+                top={637}
+                borderRadius={22}
+                maxW={"270px"}
+                bg={"white"}
+                boxShadow={"lg"}
+                overflow={"hidden"}
+                p={6}
+              >
+                <Stack spacing={0} align={"center"} mb={5}>
+                  <Heading
+                  width={147}
+                  height={130}
+                  left={919}
+                  top={637}
+                    fontSize={"6xl"}
+                    fontWeight={500}
+                    mb={5}
+                    fontFamily={"body"}
+                  >
+                     <img 
+                      src={cognac}
+                      alt="pt_menu"
+                     />
+                  </Heading>
+
+                  <p>Cognac</p>
+                </Stack>
+              </Box>
+            </Center>
+          </Box>
+      </Grid>
       </Box>
+      
+        
       <Footer />
     </div>
   );
